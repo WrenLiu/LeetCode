3dd26d4c112d51fe81322d32fc9c4ba75bf95d2a
JamesLiuwenliang <13114817178@163.com>
2020-02-12 22:46:56 +0900

    leetcode_804 is over.

--- leetcode_203/Recursive.java
+++ leetcode_203/Recursive.java
@@ -1,4 +1,5 @@
-class Solution {
+
+class Recursive {
 
     public ListNode removeElements(ListNode head , int val){
 
@@ -6,14 +7,20 @@ class Solution {
             return null;
         }
 
+        // 每个步骤分解 : res是除去头节点(要被删除的节点)外的,剩余链表;
         ListNode res = removeElements(head.next ,val);
         if(head.val == val){
+            // res存的是不含要被删除的头结点的链表
             return res;
         }
         else{
+            // 如果没有要删除的节点,就将head这个参数放在res链表的最前面
             head.next = res;
             return head;
         }
+
+        // head.next = removeElements(head.next,val);
+        // return head.val == val ? head.next :head;
     }
 
     public static void main(String[] args){
