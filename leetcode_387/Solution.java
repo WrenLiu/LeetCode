8b952d9503485699d4077d11fc7c8954b9cf7f72
JamesLiuwenliang <13114817178@163.com>
2020-03-07 15:18:32 +0900

    add main()

--- leetcode_387/Solution.java
+++ leetcode_387/Solution.java
@@ -1,20 +1,25 @@
-class Solution {
-    public int firstUniqChar(String s) {
-        int[] freq = new int[26];
+package leetcode_387;
+public class Solution {
+    public static int firstUniqChar(final String s) {
+        final int[] freq = new int[26];
 
-        for(int i=0 ;i<s.length() ;i++){
-            freq[s.charAt(i) - 'a' ] ++;
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
         }
 
-        for(int i=0 ;i<s.length() ;i++){
-            if( freq[s.charAt(i) - 'a' ]  == 1){
-                return  i;
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
             }
         }
 
-        return -1 ;
 
+        return -1;
+    }
 
-
+    public static void main(String[] args) {
+        firstUniqChar("loveleetcode");
+        
     }
 }
