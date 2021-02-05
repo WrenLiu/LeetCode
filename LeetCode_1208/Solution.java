6f42e737d0988871eb7285d0407e0f715a30cf56
JamesLiuwenliang <13114817178@163.com>
2021-02-05 21:37:42 +0900

    LeetCode1208

---
+++ LeetCode_1208/Solution.java
@@ -0,0 +1,71 @@
+
+
+/**
+ * 滑动窗口模板：
+ * ```Python
+ * def findSubArray(nums):
+ *     N = len(nums) # 数组/字符串长度
+ *     left, right = 0, 0 # 双指针，表示当前遍历的区间[left, right]，闭区间
+ *     sums = 0 # 用于统计 子数组/子区间 是否有效，根据题目可能会改成求和/计数
+ *     res = 0 # 保存最大的满足题目要求的 子数组/子串 长度
+ *     while right < N: # 当右边的指针没有搜索到 数组/字符串 的结尾
+ *         sums += nums[right] # 增加当前右边指针的数字/字符的求和/计数
+ *         while 区间[left, right]不符合题意：# 此时需要一直移动左指针，直至找到一个符合题意的区间
+ *             sums -= nums[left] # 移动左指针前需要从counter中减少left位置字符的求和/计数
+ *             left += 1 # 真正的移动左指针，注意不能跟上面一行代码写反
+ *         # 到 while 结束时，我们找到了一个符合题意要求的 子数组/子串
+ *         res = max(res, right - left + 1) # 需要更新结果
+ *         right += 1 # 移动右指针，去探索新的区间
+ *     return res
+ * ```    
+ *
+
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+        String s = "krrgw", t = "zjxss";
+        int cost = 19;
+
+//        System.out.println(equalSubstring(s,t,cost));/
+        
+
+    }
+
+
+    public static int equalSubstring(String s, String t, int maxCost) {
+
+        if(s== null || t == null || maxCost < 0 || s.length() != t.length()){
+            return 0;
+        }
+
+        int total = 0;
+
+        int left = 0,right = 0;
+        int res = 0;
+
+        while(left < s.length() && right < s.length()){
+            total += Math.abs(s.charAt(right) - t.charAt(right));
+            while(total > maxCost){
+                total -= Math.abs(s.charAt(left) - t.charAt(left));
+                left++;
+            }
+            res = Math.max(right-left+1,res);
+            right++;
+
+        }
+
+
+
+
+
+        return res;
+
+
+
+
+    }
+
+
+}
