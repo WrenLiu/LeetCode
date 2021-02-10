e040076645f09b095aa54b10b4d1b9a31cd95a7d
JamesLiuwenliang <13114817178@163.com>
2021-02-10 21:27:36 +0900

    LeetCode978

--- leetcode_978/Solution.java
+++ leetcode_978/Solution.java
@@ -17,7 +17,7 @@ public class Solution {
         for (int i = 1; i < arr.length; i++) {
             
             if(arr[i] > arr[i-1]){
-                up = down = 1;
+                up = down + 1;
                 down = 1;
             }else if(arr[i] < arr[i-1]){
                 down = up + 1;
