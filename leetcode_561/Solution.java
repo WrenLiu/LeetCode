ccc4b13eb9160c37f629d430b72d1bfa3ba0b8d6
JamesLiuwenliang <13114817178@163.com>
2021-02-16 01:15:21 +0900

    leetcode561

---
+++ leetcode_561/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+
+        if(nums == null || nums.length == 1){
+            return 0;
+        }
+
+        Arrays.sort(nums);
+
+        int total = 0;
+        for(int i =0;i<nums.length;i+=2){
+            total += nums[i];
+        }
+        return total;
+        
+
+
+
+    }
+}
