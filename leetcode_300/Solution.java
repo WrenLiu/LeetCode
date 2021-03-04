58b11e4ee89eab63e893a5d639a6c20005a923d1
JamesLiuwenliang <13114817178@163.com>
2021-03-04 17:49:20 +0900

    leetcode300

--- leetcode_300/Solution.java
+++ leetcode_300/Solution.java
@@ -1,109 +1,83 @@
 
 
-import java.lang.reflect.Array;
-import java.util.*;
+import java.util.Arrays;
 
 public class Solution {
 
     public static void main(String[] args) {
-        int[][] envelopes = {{1,2},{2,3},{3,4},{3,5},{4,5},{5,5},{5,6},{6,7},{7,8}};
-//        int[][] envelopes = {{46,89},{50,53},{52,68},{72,45},{77,81}};
-        System.out.println(maxEnvelopes_BinarySearch(envelopes));
+        int[] nums = {1,3,6,7,9,4,10,5,6};
+
+        System.out.println(lengthOfLIS_BinarySearch(nums));
 
     }
 
-    public static int maxEnvelopes_BinarySearch(int[][] nums){
-        if(nums == null || nums.length == 0){
+    public static int lengthOfLIS_BinarySearch(int[] nums){
+
+        int maxL = 0;
+        int[] dp = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int left = 0,right = maxL;
+
+            while(left < right) {
+                int mid = left + (right - left) / 2;
+                if (dp[mid] < nums[i]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+
+            }
+
+            dp[left] = nums[i];
+            if (left == maxL) {
+                maxL++;
+            }
+        }
+
+        System.out.println(Arrays.toString(dp));
+        return maxL;
+
+
+    }
+
+    public static int lengthOfLIS(int[] nums) {
+
+        if(nums == null && nums.length == 0){
             return 0;
         }
 
         int[] dp = new int[nums.length];
 
-        Arrays.sort(nums, (a, b) -> (a[0] == b[0] ? b[1]-a[1] : a[0]-b[0]));
-
-        int maxLen = 0;
-
-
-        for (int i = 0; i < nums.length; i++) {
-
-            int h = nums[i][1];
-
-            int left = 0,right = maxLen;
-
-            while(left < right){
-
-                int mid = left + (right-left)/2;
-
-                if(dp[mid] < h){
-                    left = mid+1;
-                }else{
-                    right = mid;
-                }
-
-            }
-            dp[left] = h;
-            if(left == maxLen){
-                maxLen++;
-            }
-
-
-
-
-        }
-
-        return maxLen;
-
-
-
-
-
-
-    }
-
-
-    public static int maxEnvelopes(int[][] envelopes) {
-
-        if(envelopes == null || envelopes.length == 0){
-            return 0;
-        }else if(envelopes.length == 1){
-            return 1;
-        }
-
-        Arrays.sort(envelopes, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b){
-                if(a[0]==b[0]){
-                    return a[1] - b[1];
-                }else {
-                    return a[0] - b[0];
-                }
-            }
-        });
-
-//        for (int i = 0; i < envelopes.length; i++) {
-//            System.out.println(envelopes[i][0]+" "+envelopes[i][1]);
-//        }
-
-        int[] dp = new int[envelopes.length];
-        int maxLen = 1;
-
         for (int i = 0; i < dp.length; i++) {
             dp[i] = 1;
         }
+        int maxLen = 1;
 
-        for (int i = 1; i < envelopes.length; i++) {
+
+        for (int i = 1; i < nums.length; i++) {
+
             for (int j = 0; j < i; j++) {
-                if(envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1]){
-                    dp[i] = Math.max(dp[i],dp[j]+1 );
+
+                if(nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
-                maxLen = Math.max(maxLen,dp[i]);
-                
             }
+
+            maxLen = Math.max(maxLen,dp[i]);
+
         }
+
+        Arrays.sort(dp);
+
+
+
+
 
 
 
         return maxLen;
-
 
     }
 
