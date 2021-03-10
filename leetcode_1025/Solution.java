a65dce17df5ec6b3a66235229cd11f97f839a076
JamesLiuwenliang <13114817178@163.com>
2021-03-10 23:14:36 +0900

    leetcode1025

---
+++ leetcode_1025/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+
+    public static void main(String[] args) {
+
+        System.out.println(divisorGame(3));
+
+    }
+
+    private static boolean divisorGame(int N) {
+
+
+        return N%2 == 0 ? true : false;
+
+
+    }
+
+}
