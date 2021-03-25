bfd35fe5caf5bad79d78468433787ceb0b6ef526
JamesLiuwenliang <13114817178@163.com>
2021-03-25 20:16:49 +0900

    leetcode82

--- leetcode_82/Solution.java
+++ leetcode_82/Solution.java
@@ -30,17 +30,30 @@ public class Solution {
         }
 
         ListNode82 next = head.next;
+        
+        //如果是这种情况
+        //      1 --> 1 --> 1 --> 2 --> 3
+        //     head  next
+        //1.则需要移动next直到出现与当前head.value不相等的情况（含null）
+        //2.并且此时的head已经不能要了，因为已经head是重复的节点
+        //--------------else-------------
+        //      1 --> 2 --> 3
+        //     head  next
+        //3.如果没有出现1的情况，则递归返回的节点就作为head的子节点
 
         if(head.val == next.val){
-
+            //1
             while(next != null && next.val == head.val){
                 next = next.next;
             }
 
+            // head 此时已经不能要了
+            // 2
             head = deleteDuplicates_Leetcode(head);
 
 
         }else {
+            // 3
             head.next = deleteDuplicates_Leetcode(next);
         }
 
