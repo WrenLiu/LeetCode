5b9c9660a467c3a0573208369d2636f65968afee
JamesLiuwenliang <13114817178@163.com>
2021-04-25 19:09:26 +0900

    leetcode5739

---
+++ leetcode_5739/Solution.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,4,8,13};
+
+        System.out.println(maxFrequency_leetcode(nums,5));
+
+    }
+
+
+    public static int maxFrequency_leetcode(int[] nums, int k){
+
+
+        if(nums == null ){
+            return 0;
+        }
+
+        Arrays.sort(nums);
+
+        int leftIndex = 0, rightIndex = 1;
+
+        int len = nums.length;
+
+        int res = 1;
+        int total = 0;
+
+
+        while(rightIndex < len){
+            total += (nums[rightIndex] - nums[rightIndex - 1])*(rightIndex - leftIndex);
+
+            while (total > k){
+                total -= nums[rightIndex] - nums[leftIndex];
+                leftIndex++;
+            }
+
+            res = Math.max(res , rightIndex - leftIndex + 1);
+            rightIndex++;
+
+        }
+
+        return res;
+
+    }
+
+}
