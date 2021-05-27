29a4dd194a078df185e0316a7e4c360ca972df21
JamesLiuwenliang <13114817178@163.com>
2021-05-27 14:53:04 +0900

    leetcode461

--- leetcode_461/Solution.java
+++ leetcode_461/Solution.java
@@ -9,6 +9,18 @@ public class Solution {
         System.out.println(hammingDistance(x,y));
     }
 
+    public int hammingDistance_best(int x, int y) {
+
+
+		int z = x ^ y;
+		z = (z & 0x55555555) + ((z >> 1) & 0x55555555);
+		z = (z & 0x33333333) + ((z >> 2) & 0x33333333);
+		z = (z & 0x0f0f0f0f) + ((z >> 4) & 0x0f0f0f0f);
+		z = (z & 0x00ff00ff) + ((z >> 8) & 0x00ff00ff);
+		z = (z & 0x0000ffff) + ((z >> 16) & 0x0000ffff);
+		return z;
+	}
+
     public static int hammingDistance_leetcode(int x, int y) {
 
         int ans = 0;
